b26bf14f64498e9547d03e3b71eed79574ac7f81
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-07 15:11:45 -0300

    Criacao da classe Gerente.

--- src/main.kt
+++ src/main.kt
@@ -4,12 +4,29 @@ fun main() {
     val bruno = Funcionario(
         nome = "Bruno",
         cpf = "111.111.111-11",
-        salario = 1000.0,
-        tipo = 2
+        salario = 1000.0
     )
 
     println("nome ${bruno.nome}")
     println("cpf ${bruno.cpf}")
     println("salario ${bruno.salario}")
     println("bonificação ${bruno.bonificacao()}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.0,
+        senha = 1234
+    )
+
+    println("nome ${fran.nome}")
+    println("cpf ${fran.cpf}")
+    println("salario ${fran.salario}")
+    println("bonificação ${fran.bonificacao()}")
+
+    if (fran.autentica(1234)){
+        println("autenticou com sucesso")
+    }else{
+        println("falha na autenticação")
+    }
 }
