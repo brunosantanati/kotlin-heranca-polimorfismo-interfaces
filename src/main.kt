15a96581c330762d8c661b997cd704b4bc64f740
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-07 16:47:42 -0300

    Fazendo Gerente e Diretor herdar de Funcionario.

--- src/main.kt
+++ src/main.kt
@@ -29,4 +29,24 @@ fun main() {
     }else{
         println("falha na autenticação")
     }
+
+    val gui = Diretor(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("nome ${gui.nome}")
+    println("cpf ${gui.cpf}")
+    println("salario ${gui.salario}")
+    println("bonificação ${gui.bonificacao()}")
+    println("plr ${gui.plr}")
+
+    if (gui.autentica(4000)){
+        println("autenticou com sucesso")
+    }else{
+        println("falha na autenticação")
+    }
 }
