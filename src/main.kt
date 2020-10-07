f7cdc216be992e551a3ec1844829e3902fba2037
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2020-10-07 14:28:36 -0300

    Criada classe Funcionario.

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,14 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val bruno = Funcionario(
+        nome = "Bruno",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+
+    println("nome ${bruno.nome}")
+    println("cpf ${bruno.cpf}")
+    println("salario ${bruno.salario}")
+    println("bonificação ${bruno.bonificacao()}")
 }
